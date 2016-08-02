80cce1996bc09ccf90df8801fda5fd7a544219aa
Paul Henderson <paulyk1162@hotmail.co.uk>
2016-08-02 17:20:27 +0000

    Added request header parser functionality

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/app/whoami", function(req, res){
+    var whoami = {
+        ipaddress : req.ip,
+        language : req.headers["accept-language"],
+        software : req.headers["user-agent"]
+    }
+    
+    res.write(JSON.stringify(whoami));
+    res.end();
+})
+
+app.listen(8080, function(){
+    console.log("I'm listening.");
+})
